8c11912d5ddfff35a35925250720db8bc843ecf0
oomis <damilolaomiyera@yahoo.com>
2019-10-18 18:38:32 +0100

    making it better

---
+++ build/precache-manifest.d65aedac3b79cd1cfbfb5b61138d7395.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca03f7f601bbec0246e276854569db7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc9a5802ca340c4d92d",
+    "url": "/static/css/3.56d65a16.chunk.css"
+  },
+  {
+    "revision": "d35f0eda29ce49811592",
+    "url": "/static/css/main.f9941296.chunk.css"
+  },
+  {
+    "revision": "b86826a3c566adf00e97",
+    "url": "/static/js/2.7a374c27.chunk.js"
+  },
+  {
+    "revision": "0bc9a5802ca340c4d92d",
+    "url": "/static/js/3.35f2a3be.chunk.js"
+  },
+  {
+    "revision": "d3616a97390d26ca5199",
+    "url": "/static/js/4.2bf2e10f.chunk.js"
+  },
+  {
+    "revision": "d35f0eda29ce49811592",
+    "url": "/static/js/main.5783aa43.chunk.js"
+  },
+  {
+    "revision": "f58b8964ef0fcecb3a91",
+    "url": "/static/js/runtime-main.7017f72b.js"
+  },
+  {
+    "revision": "15564ef2f0cb97622307a37f598aa2d3",
+    "url": "/static/media/Image2.15564ef2.svg"
+  },
+  {
+    "revision": "250b99d56fb052a853cb8d4c7cd261dc",
+    "url": "/static/media/aboutScreen.250b99d5.svg"
+  },
+  {
+    "revision": "3755bb099364bb36cbddc28eb1888e28",
+    "url": "/static/media/analytics.3755bb09.svg"
+  },
+  {
+    "revision": "913468c7d9e27f3ea1e7693408b39a63",
+    "url": "/static/media/brandvat_white.913468c7.jpeg"
+  },
+  {
+    "revision": "3d1a3f907c69b56cf116d3a2e22b5141",
+    "url": "/static/media/content.3d1a3f90.svg"
+  },
+  {
+    "revision": "5c46b5b80adff7ccd638d9d92ad2afe1",
+    "url": "/static/media/email.5c46b5b8.svg"
+  },
+  {
+    "revision": "419b03f6c9bc0fc04824055c6ff4730e",
+    "url": "/static/media/graphic.419b03f6.svg"
+  },
+  {
+    "revision": "2f0d69a7364987a0b5d311e6a14a4b54",
+    "url": "/static/media/iteam4.2f0d69a7.jpg"
+  },
+  {
+    "revision": "d5802c0cf35938c5d4daeda6d26f2f06",
+    "url": "/static/media/item1.d5802c0c.jpg"
+  },
+  {
+    "revision": "5e1724260457f6ded4af191c1b4c1fb1",
+    "url": "/static/media/item2.5e172426.jpeg"
+  },
+  {
+    "revision": "0a09d8a8595a9096a2e943e071c7297a",
+    "url": "/static/media/item3.0a09d8a8.jpg"
+  },
+  {
+    "revision": "c9c8d2f5363abedaccbf76a4e3d7fda1",
+    "url": "/static/media/logo_purple.c9c8d2f5.jpeg"
+  },
+  {
+    "revision": "668855ce91d18db5f4c5ce566e1c5343",
+    "url": "/static/media/paid.668855ce.svg"
+  },
+  {
+    "revision": "fb7c27bd8073c1f7ece286fbfddd400e",
+    "url": "/static/media/seo.fb7c27bd.svg"
+  },
+  {
+    "revision": "aa1a77cef29b3697bef9ae91aeab177a",
+    "url": "/static/media/social-media.aa1a77ce.svg"
+  },
+  {
+    "revision": "c4aeef32cbdac71988b00a9ca7d389f4",
+    "url": "/static/media/web.c4aeef32.svg"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, ".", "build")));
+
+app.post("/contact/message", (req, res) => {
+  console.log(req.body);
+  const { name, service, telePhone, email, message } = req.body;
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "Brandvatdigital@gmail.com",
+      pass: "11aspilicueta11"
+    }
+  });
+
+  var mailOptions = {
+    from: "Brandvatdigital@gmail.com",
+    to: `${email}`,
+    subject: "Thank You for contacting us",
+    text: `
+      Your message has been recieved, we will contact you soonest.
+
+      Yours Sincerely,
+      BrandVat Team.
+    `
+  };
+
+  var mailOptionss = {
+    from: "Brandvatdigital@gmail.com",
+    to: `Brandvatdigital@gmail.com`,
+    subject: `New message from ${email} via website`,
+    text: `
+     ${name} has sent a message to you from the website.
+     Here are the details.
+     Name: ${name}
+     Email: ${email}
+     Telephone: ${telePhone}
+     service interested in: ${service}
+     How we want to be Help: ${message}
+
+     Yours Sincerely,
+     BrandVat Team.
+    `
+  };
+
+  transporter.sendMail(mailOptionss, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      return res.json({ err: "message not sent" });
+    } else {
+      console.log("Email sent: " + info.response);
+      return res.json({ message: { name: email, data: info.response } });
+    }
+  });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, ".", "build", "index.html"));
+});
+
+const port = process.env.PORT || 9001;
+app.listen(port);
+
+console.log(`express app listening on port ${port}`);
