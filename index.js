a09acb9d4150d84056340add1d76e5581e436628
oomis <damilolaomiyera@yahoo.com>
2019-10-22 18:06:16 +0100

    first commit

--- index.js
+++ index.js
@@ -10,36 +10,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, ".", "build")));
 
 app.post("/contact/message", (req, res) => {
-  console.log(req.body);
-  const { name, service, telePhone, email, message } = req.body;
-  let transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "Brandvatdigital@gmail.com",
-      pass: "11aspilicueta11"
-    }
-  });
+    console.log(req.body);
+    const { name, service, telePhone, email, message } = req.body;
+    let transporter = nodemailer.createTransport({
+        service: "Gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: "Brandvatdigital@gmail.com",
+            pass: "11aspilicueta11"
+        }
+    });
 
-  var mailOptions = {
-    from: "Brandvatdigital@gmail.com",
-    to: `${email}`,
-    subject: "Thank You for contacting us",
-    text: `
+    var mailOptions = {
+        from: "Brandvatdigital@gmail.com",
+        to: `${email}`,
+        subject: "Thank You for contacting us",
+        text: `
       Your message has been recieved, we will contact you soonest.
 
       Yours Sincerely,
       BrandVat Team.
     `
-  };
+    };
 
-  var mailOptionss = {
-    from: "Brandvatdigital@gmail.com",
-    to: `Brandvatdigital@gmail.com`,
-    subject: `New message from ${email} via website`,
-    text: `
+    var mailOptionss = {
+        from: "Brandvatdigital@gmail.com",
+        to: `Brandvatdigital@gmail.com`,
+        subject: `New message from ${email} via website`,
+        text: `
      ${name} has sent a message to you from the website.
      Here are the details.
      Name: ${name}
@@ -51,32 +51,32 @@ app.post("/contact/message", (req, res) => {
      Yours Sincerely,
      BrandVat Team.
     `
-  };
+    };
 
-  transporter.sendMail(mailOptionss, function(error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    transporter.sendMail(mailOptionss, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-      return res.json({ err: "message not sent" });
-    } else {
-      console.log("Email sent: " + info.response);
-      return res.json({ message: { name: email, data: info.response } });
-    }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+            return res.json({ err: "message not sent" });
+        } else {
+            console.log("Email sent: " + info.response);
+            return res.json({ message: { name: email, data: info.response } });
+        }
+    });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, ".", "build", "index.html"));
+    res.sendFile(path.join(__dirname, ".", "build", "index.html"));
 });
 
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log(`express app listening on port ${port}`);
